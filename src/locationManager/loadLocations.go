a39ee96618f666ad0f19024bfef1c29c37727fd7
codethings <kirinmurphy@gmail.com>
2024-08-09 20:39:05 -0500

    Homepage (#2)
    * a bit of cleanup
    
    * created homepage wizard to create map

--- src/locationManager/loadLocations.go
+++ src/locationManager/loadLocations.go
@@ -1,4 +1,4 @@
-package locationHelpers
+package locationManager
 
 import (
 	"fmt"
--- src/router/homeRouteHandler.go
+++ src/router/homeRouteHandler.go
@@ -11,16 +11,17 @@ import (
 func homeRouteHandler(w http.ResponseWriter, r *http.Request, redisClient *redis.Client, ctx context.Context) {
 	sheetId := r.URL.Query().Get("sheetId")
 	if sheetId == "" {
-		http.Error(w, "Missing sheetId parameter", http.StatusBadRequest)
+		tmpl := template.Must(template.ParseFiles("src/templates/home.html"))
+		tmpl.Execute(w, nil)
 		return
 	}
 
 	_, err := redisClient.Get(ctx, sheetId).Result()
 	if err == redis.Nil {
-		http.Error(w, "No locations found", http.StatusNotFound)
+		http.Error(w, "Could not find spreadsheet id ${sheetId}", http.StatusNotFound)
 		return
 	}
 
-	tmpl := template.Must(template.ParseFiles("src/templates/index.html"))
+	tmpl := template.Must(template.ParseFiles("src/templates/map.html"))
 	tmpl.Execute(w, map[string]string{"SheetId": sheetId})
 }
