ee9a343b78f55301cbf627c5b9a44c4d0c01c557
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-08-13 00:11:44 -0500

    bind copy to grab sample and grab email

--- src/templates/js/bindCopyListener.js
+++ src/templates/js/bindCopyListener.js
@@ -1,17 +1,23 @@
-function bindCopyListener ({ elementId, contentToCopyId }) {
+function bindCopyListener (props) {
+  const { 
+    elementId, 
+    contentToCopyId, 
+    copyMessage = 'Copied!' 
+  } = props;
+
   const copyButton = document.getElementById(elementId);
 
   copyButton.addEventListener('click', function() {
     const contentToCopy = contentToCopyId 
       ? document.getElementById(contentToCopyId).textContent
-      : copyButton.getAttribute('copy-target');
+      : copyButton.getAttribute('text-to-copy');
 
     navigator.clipboard.writeText(contentToCopy)
       .then(() => {
         const copyButton = document.getElementById(elementId);
-        const originalText = copyButton.textContent;
-        copyButton.textContent = 'Copied!';
-        setTimeout(() => copyButton.textContent = originalText, 2000); // Revert after 2 seconds
+        const originalText = copyButton.innerHTML;
+        copyButton.innerHTML = copyMessage;
+        setTimeout(() => copyButton.innerHTML = originalText, 2000); 
       })
       .catch(err => {
         console.error('Failed to copy text: ', err);
