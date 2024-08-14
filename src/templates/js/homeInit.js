05448f7e4e80715fa45940c5ef7961d2f2839cab
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-08-14 09:31:13 -0500

    cleanup

---
+++ src/templates/js/homeInit.js
@@ -0,0 +1,76 @@
+(function () {
+  const validateUrl = (url) => {
+    const regex = /^https:\/\/docs\.google\.com\/spreadsheets\/d\/e\/([a-zA-Z0-9-_]+)\/pubhtml$/;
+    return regex.test(url) ? url.match(regex)[1] : null;
+  };
+
+  const displayError = (message) => {
+    const errorElement = document.getElementById("urlError");
+    errorElement.textContent = message;
+    errorElement.classList.remove("hidden");
+  };
+
+  const hideError = () => {
+    document.getElementById("urlError").classList.add("hidden");
+  };
+
+  document.getElementById("sheetUrl").addEventListener("paste", (event) => {
+    setTimeout(() => {
+      const url = event.target.value;
+      const sheetId = validateUrl(url);
+      if (!sheetId) {
+        displayError("I think you got a bad URL there bud.");
+        console.log('SHEETID', sheetId);
+
+        return;
+      } else {
+        hideError();
+      }
+
+      document.getElementById("loadingIndicator").classList.remove("hidden");
+
+      htmx.ajax('POST', '/updateMapUI', { 
+        values: { sheetId },
+        target: '#mapContainer',
+        swap: 'innerHtml'
+      });
+      
+    }, 0);
+  });
+
+  document.getElementById("sheetForm").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); 
+    }
+  });
+
+  document.body.addEventListener("htmx:afterSwap", (event) => {
+    setTimeout(() => {
+      const mapContainer = document.getElementById("mapContainer");
+      document.getElementById("loadingIndicator").classList.add("hidden");
+      if (mapContainer) {
+        const topPosition = mapContainer.getBoundingClientRect().top + window.scrollY - 10;
+        window.scrollTo({
+          top: topPosition,
+          behavior: "smooth"
+        });
+      }
+    }, 50);
+  });
+
+  const demoPrompt = document.getElementById("demoPrompt");
+  if (demoPrompt) {
+    const pasteDemoUrlButton = document.getElementById("pasteDemoUrl");
+    const sheetUrlInput = document.getElementById("sheetUrl");
+    pasteDemoUrlButton.addEventListener("click", () => {
+      const urlToPaste = pasteDemoUrlButton.getAttribute("text-to-copy");
+      sheetUrlInput.value = urlToPaste;
+      sheetUrlInput.dispatchEvent(new Event("paste"));
+    });
+  }
+
+  bindCopyListener({ 
+    elementId: 'copy-email', 
+    copyMessage: 'Email copied!' 
+  });  
+})()
--- src/templates/js/mapInit.js
+++ src/templates/js/mapInit.js
@@ -1,54 +1,57 @@
-const map = L.map('map', {
-  maxZoom: 18
-}).setView([0, 0], 2);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
-
-const redIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-  className: 'red-marker' // Add a custom class to apply CSS
-});
-
-const markers = L.markerClusterGroup();
-
-htmx.on('htmx:afterSettle', function(evt) {
-  const locations = JSON.parse(evt.detail.xhr.response);
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', locations);
-
-  locations.forEach(function(loc) {
-    const { name, address, city, state, country, website, phoneNumber } = loc;
-    const markerOptions = loc.isCo404Loc ? { icon: redIcon } : {};
-    const args = [loc.latitude, loc.longitude];
-
-    const formattedWebsite = website ? website.replace(/^(https?:\/\/|\/\/)/, '') : null;
-
-    const popupContent = `
-      <div class="popup-template">
-        <h3 class="text-lg font-bold">${name}</h3>
-        <div class="text-md">
-          <div>${address}</div>
-          <div>${city}${!!state ? ' ' : ''}${state}, ${country}</div>
-          <div class="${!website ? 'hidden' : ''}">
-            <a href="https://${formattedWebsite}" target="_blank" rel="noopener noreferrer">${website}</a>
-          </div>
-          <div class="${!phoneNumber ? 'hidden' : ''}">
-            <a href="tel:${phoneNumber}" target="_blank" rel="noopener noreferrer">${phoneNumber}</a>
+(function () {
+  const map = L.map('map', {
+    maxZoom: 18
+  }).setView([0, 0], 2);
+  
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+  
+  const redIcon = new L.Icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+    className: 'red-marker' // Add a custom class to apply CSS
+  });
+  
+  const markers = L.markerClusterGroup();
+  
+  htmx.on('htmx:afterSettle', function(evt) {
+    const locations = JSON.parse(evt.detail.xhr.response);
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', locations);
+  
+    locations.forEach(function(loc) {
+      const { name, address, city, state, country, website, phoneNumber } = loc;
+      const markerOptions = loc.isCo404Loc ? { icon: redIcon } : {};
+      const args = [loc.latitude, loc.longitude];
+  
+      const formattedWebsite = website ? website.replace(/^(https?:\/\/|\/\/)/, '') : null;
+  
+      const popupContent = `
+        <div class="popup-template">
+          <h3 class="text-lg font-bold">${name}</h3>
+          <div class="text-md">
+            <div>${address}</div>
+            <div>${city}${!!state ? ' ' : ''}${state}, ${country}</div>
+            <div class="${!website ? 'hidden' : ''}">
+              <a href="https://${formattedWebsite}" target="_blank" rel="noopener noreferrer">${website}</a>
+            </div>
+            <div class="${!phoneNumber ? 'hidden' : ''}">
+              <a href="tel:${phoneNumber}" target="_blank" rel="noopener noreferrer">${phoneNumber}</a>
+            </div>
           </div>
         </div>
-      </div>
-    `;
+      `;
+    
+      const marker = L.marker(args, markerOptions).bindPopup(popupContent);
+      markers.addLayer(marker);
+    });
   
-    const marker = L.marker(args, markerOptions).bindPopup(popupContent);
-    markers.addLayer(marker);
+    map.addLayer(markers);
+    
+    const group = new L.featureGroup(markers.getLayers());
+    map.fitBounds(group.getBounds());
   });
-
-  map.addLayer(markers);
   
-  const group = new L.featureGroup(markers.getLayers());
-  map.fitBounds(group.getBounds());
-});
+})() 
