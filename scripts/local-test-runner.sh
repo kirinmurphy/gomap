336b5e4020ded86e78056334466f5887ad3cede1
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-08-25 00:04:51 -0500

    test runner cleanup

--- scripts/local-test-runner.sh
+++ scripts/local-test-runner.sh
@@ -1,14 +1,19 @@
 #!/bin/bash 
 
-output=$(go test -count=1 ./src/... 2>&1)
+output=$(go test -count=1 -v ./src/... 2>&1)
 exit_code=$?
 
 filtered_output=$(echo "$output" | grep -v '\[no test files\]')
 
-if [ $exit_code -ne 0 ]; then
-  echo -e "\033[1;91m$filtered_output\033[0m"
-  exit $exit_code
-fi
+fail_lines=$(echo "$filtered_output" | grep "FAIL:")
 
-echo -e "\033[1;92mTESTS: OK ツ\033[0m"
-  
+if [ $exit_code -ne 0 ]; then
+  filtereder_ouput=$(echo "$filtered_output" | grep -v "FAIL:")
+  echo -e "\033[1;91m$fail_lines\033[0m"
+  echo "$filtereder_ouput"
+  echo -e "\033[1;91m===============================\033[0m"
+  exit $exit_code
+else
+  echo "$filtered_output"
+  echo -e "\033[1;92mTESTS: OK ツ\033[0m"
+fi
--- src/router/tests/homeRoute_test.go
+++ src/router/tests/homeRoute_test.go
@@ -12,12 +12,19 @@ import (
 	"golang.org/x/net/html"
 )
 
-func TestHomeRouter(t *testing.T) {
+var (
+	demoPrompt      = "You are using the demo version."
+	homepageTitle   = "MAPPERBOI BETA"
+	homepageTagline = "Website Map Generator"
+	mapIdString     = "id=\"map\""
+)
+
+func setupRouterTest(t *testing.T, queryParam string) (string, *html.Node) {
 	ctx := context.Background()
 	mockRedisClient := &testUtils.MockRedisClient{}
 	router := router.InitRouter(mockRedisClient, ctx)
 
-	req, err := http.NewRequest("GET", "/", nil)
+	req, err := http.NewRequest("GET", "/"+queryParam, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -29,16 +36,42 @@ func TestHomeRouter(t *testing.T) {
 		t.Errorf("Expected status code %d, got %d", http.StatusOK, routerRecorder.Code)
 	}
 
-	doc, err := html.Parse(strings.NewReader(routerRecorder.Body.String()))
+	stringifiedDoc := routerRecorder.Body.String()
+	htmlDoc, err := html.Parse(strings.NewReader(stringifiedDoc))
 	if err != nil {
 		t.Fatalf("Failed to parse HTML: %v", err)
 	}
 
-	// var sb strings.Builder
-	// err = html.Render(&sb, doc)
-	// if err != nil {
-	// 	t.Fatalf("Failed to render HTML: %v", err)
-	// }
+	return stringifiedDoc, htmlDoc
+}
 
-	testUtils.CheckElement(t, doc, "h1", "MAPPERBOI BETA")
+func TestHomeRouter(t *testing.T) {
+	stringifiedDoc, htmlDoc := setupRouterTest(t, "")
+
+	testUtils.CheckElement(t, htmlDoc, "h1", homepageTitle)
+
+	if strings.Contains(stringifiedDoc, demoPrompt) {
+		t.Errorf("Demo section displayed without demo=true query param")
+	}
+
+	if strings.Contains(stringifiedDoc, mapIdString) {
+		t.Errorf("Homepage map state displayed without sheetId query param")
+	}
+}
+
+func TestHomeRouter_WithDemo(t *testing.T) {
+	stringifiedDoc, _ := setupRouterTest(t, "?demo=true")
+
+	if !strings.Contains(stringifiedDoc, demoPrompt) {
+		t.Errorf("Demo section NOT displayed with demo=true query param")
+	}
+}
+
+func TestHomeRouter_WithSheetId(t *testing.T) {
+	t.Skip("Skipping this test")
+	stringifiedDoc, _ := setupRouterTest(t, "?sheetId=2PACK-3kj3l2kjf32f")
+
+	if !strings.Contains(stringifiedDoc, mapIdString) {
+		t.Errorf("Map state NOT displayed with sheetId query param")
+	}
 }
